7d89a9caa3118051e3b01db2f448ddfa79330f6e
ibrahim8545 <ibrahimmostafa122020@gmail.com>
2024-08-26 04:02:33 +0300

    create splash screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:moviesapp/home_screen.dart';
+import 'package:moviesapp/utils/app_color.dart';
+
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  static const String routeName = 'splash';
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(
+        const Duration(seconds: 1),
+        () {
+         
+            Navigator.pushReplacementNamed(context, HomeScreen.routeName);
+         
+        },
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColor.primaryColor,
+      child: Column(
+        children: [
+          const Spacer(),
+          Center(
+            child: Image.asset(
+              'assets/images/movies.png',
+              height: 211,
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
