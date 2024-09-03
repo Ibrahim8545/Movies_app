839804b68a5d455838689281951be89b2e7d88f9
compu <mmeal1347@gmail.com>
2024-09-03 10:11:51 +0300

    category

--- lib/tabs/browser_tabs.dart
+++ lib/tabs/browser_tabs.dart
@@ -1,62 +1,71 @@
-
 import 'package:flutter/material.dart';
+import 'package:moviesapp/categry_details.dart';
+import 'package:moviesapp/models/movie_details_model/genre_model.dart';
+import 'package:moviesapp/utils/api_categry.dart';
+
 
 class BrowserTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        title:Text(
-          'Browse Category',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-      backgroundColor: Colors.black,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+        title: Text('Browse ',style: TextStyle(
+         color: Colors.white,
+         fontSize: 30
 
-            SizedBox(height: 20),
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  childAspectRatio: 16 / 9,
-                ),
-                itemCount: 16,
-                itemBuilder: (context, index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/Image.png'),
-                        fit: BoxFit.cover,
+        ),),
+      ),
+      body: FutureBuilder<List<Genre>>(
+        future: ApiService().fetchGenres(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No categories found'));
+          } else {
+            return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 2,
+              ),
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final genre = snapshot.data![index];
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => GenreDetailPage(genre: genre,),
                       ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Action',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+                    );
+                  },
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/0e34a5e080e8c915030603ddcdb4eeba.png',
+                       height: 500,
+                          width: 200,
+                      ),
+                      Container(
+                        color: Colors.black54,
+                        child: Text(
+                          genre.name,
+                          style: TextStyle(color: Colors.white, fontSize: 20),
                         ),
                       ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
